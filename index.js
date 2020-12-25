6ef3c5a85edf92af8f4b4b329be6f1183c60e5e4
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2020-12-25 17:56:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,29 +9,24 @@ gsap.utils.toArray( '#mainHero' ).forEach( section => {
     } );
 } );
 
-let proxy = { skew: 0 },
-  skewSetter = gsap.quickSetter(".skewElem", "skewY", "deg"), // fast
-  clamp = gsap.utils.clamp(-7, 7); // don't let the skew go beyond 20 degrees.
 
-ScrollTrigger.create({
-  onUpdate: (self) => {
-    let skew = clamp(self.getVelocity() / -300);
+let proxy = { skew: 0 },
+  skewSetter = gsap.quickSetter( ".transition3", "skewY", "deg" ), // fast
+  clamp = gsap.utils.clamp( -5, 5 ); // don't let the skew go beyond 20 degrees. 
+
+ScrollTrigger.create( {
+  onUpdate: ( self ) => {
+    let skew = clamp( self.getVelocity() / -650 );
     // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
-    if (Math.abs(skew) > Math.abs(proxy.skew)) {
+    if ( Math.abs( skew ) > Math.abs( proxy.skew ) ) {
       proxy.skew = skew;
-      gsap.to(proxy, {
-        skew: 0,
-        duration: 0.8,
-        ease: "power3",
-        overwrite: true,
-        onUpdate: () => skewSetter(proxy.skew),
-      });
+      gsap.to( proxy, { skew: 0, duration: 0.8, ease: "power3", overwrite: true, onUpdate: () => skewSetter( proxy.skew ) } );
     }
-  },
-});
+  }
+} );
 
 // make the right edge "stick" to the scroll bar. force3D: true improves performance
-gsap.set(".work__left", { transformOrigin: "right center", force3D: true });
+gsap.set( ".transition3", { transformOrigin: "right center", force3D: true } );
 
 
 gsap.from("#me", {
