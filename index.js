14810b30f016a457c8e2a8afb1028dd24f55cd99
Ashfaqe Ahmed <ashfaqe.sa12@gmail.com>
2020-12-26 14:00:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -35,17 +35,17 @@ gsap.set( ".transition3, .featuredh4, #projects", { transformOrigin: "right cent
 //Smooth transitions
 
 // usage:
-batch( ".transition2, .transition1", {
+batch(".transition1, .transition2", {
   interval: 0.1, // time window (in seconds) for batching to occur. The first callback that occurs (of its type) will start the timer, and when it elapses, any other similar callbacks for other targets will be batched into an array and fed to the callback. Default is 0.1
-  batchMax: 53,   // maximum batch size (targets)
-  onEnter: batch => gsap.to( batch, { autoAlpha: 1, stagger: 0.15, overwrite: true } ),
-  onLeave: batch => gsap.set( batch, { autoAlpha: 0, overwrite: true } ),
-  onEnterBack: batch => gsap.to( batch, { autoAlpha: 1, stagger: 0.15, overwrite: true } ),
-  onLeaveBack: batch => gsap.set( batch, { autoAlpha: 0, overwrite: true } )
+  batchMax: 3, // maximum batch size (targets)
+  onEnter: (batch) =>
+    gsap.to(batch, { autoAlpha: 1, stagger: 0.05, overwrite: true   }),
+  onLeave: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
+  onEnterBack: (batch) =>
+    gsap.to(batch, { autoAlpha: 1, stagger: 0.05, overwrite: true }),
+  onLeaveBack: (batch) => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
   // you can also define things like start, end, etc.
-} );
-
-
+});
 
 
 // the magical helper function (no longer necessary in GSAP 3.3.1 because it was added as ScrollTrigger.batch())...
